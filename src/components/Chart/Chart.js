f1d23886c6fc0b18bf694039ef59b680f097eafa
claudiopome <claudiopome@gmail.com>
2024-01-02 16:59:19 +0100

    display chart on app; minor fix.

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,8 +4,9 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
-  const dataPointValues = props.dataPoint.map((dataPoint) => dataPoint.value);
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
   const totalMaximum = Math.max(...dataPointValues);
+
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -7,11 +7,15 @@ const ChartBar = (props) => {
 
   if (props.maxValue > 0) {
     barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+    console.log(barFillHeight);
   }
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
-        <div className="char-bar__fill" style={{ height: barFillHeight }}></div>
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
       </div>
       <div className="chart-bar__label">{props.label}</div>
     </div>
